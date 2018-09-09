57e7c4e60de8462cb51cfcbe8afff560ffca6bae
Brent Jackson <jxnblk@gmail.com>
2018-09-09 17:46:52 -0400

    Edit install text

--- src/components.js
+++ src/components.js
@@ -18,7 +18,7 @@ import {
 
 export const photo = 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'
 export const docs = '/getting-started'
-export const install = 'npm i rebass'
+export const install = 'npm i rebass@next'
 export const github = 'https://github.com/rebassjs/rebass'
 
 export const scope = {
