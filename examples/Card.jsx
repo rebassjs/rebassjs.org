7ee8ae919a66edda06fade39df6c32db2fbe0758
Brent Jackson <jxnblk@gmail.com>
2018-09-11 19:08:38 -0400

    Edit examples

--- examples/Card.jsx
+++ examples/Card.jsx
@@ -1,7 +1,7 @@
 <Card width={256}>
   <BackgroundImage
     ratio={1}
-    src='https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'
+    image='https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'
   />
   <Subhead p={2}>
     Hello
