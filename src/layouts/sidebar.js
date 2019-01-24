0ee5f741c12972c7619f816c0720cf4a79d27b27
Brent Jackson <jxnblk@gmail.com>
2019-01-24 17:03:05 -0500

    Use sidepane for navigation

--- src/layouts/sidebar.js
+++ src/layouts/sidebar.js
@@ -3,7 +3,7 @@ import { Flex, Box, Text, Link } from 'rebass'
 import styled, { ThemeProvider } from 'styled-components'
 import { graphql, StaticQuery } from 'gatsby'
 import { Location } from '@reach/router'
-import Sidebar from 'react-sidebar'
+import Sidepane from 'sidepane'
 import NavLink from '../NavLink'
 import { Burger } from 'reline'
 
@@ -25,38 +25,6 @@ const breakpoint = 'screen and (min-width:40em)'
 const initialMQ = typeof window !== 'undefined'
   ? window.matchMedia(breakpoint)
   : { matches: false }
-
-class SidebarState extends React.Component {
-  state = {
-    open: false,
-    docked: initialMQ.matches
-  }
-
-  update = fn => this.setState(fn)
-
-  handleMedia = () => {
-    this.setState({
-      docked: this.media.matches,
-      open: false
-    })
-  }
-
-  componentDidMount () {
-    this.media = window.matchMedia(breakpoint)
-    this.media.addListener(this.handleMedia)
-  }
-
-  componentWillUnmount () {
-    this.media.removeListener(this.handleMedia)
-  }
-
-  render () {
-    return this.props.render({
-      ...this.state,
-      update: this.update,
-    })
-  }
-}
 
 const removeSlash = str => str.replace(/\/$/, '')
 
@@ -119,67 +87,44 @@ const MenuButton = styled.button({
 })
 
 export default props =>
-  <SidebarState
-    render={({
-      open,
-      docked,
-      update
-    }) => (
-      <StaticQuery
-        query={query}
-        render={({ site }) => (
-          <Sidebar
-            open={open}
-            docked={docked}
-            onSetOpen={open => update({ open })}
-            shadow={false}
-            defaultSidebarWidth={256}
-            sidebar={(
-              <Box
-                width={[ 256 ]}
-                px={2}
-                py={3}
-                bg='white'
-                style={{
-                  minHeight: '100vh'
-                }}>
-                {site.siteMetadata.navigation.map(({ text, href }) => (
-                  <NavLink
-                    key={href}
-                    to={href}
-                    children={text}
-                  />
-                ))}
-                <Box my={4} />
-                <NavLink href='https://github.com/rebassjs/rebass' children='GitHub' />
-                <NavLink href='https://jxnblk.com' children='Made by Jxnblk' />
-              </Box>
-            )}>
-            {!docked && (
-              <MenuButton
-                onClick={e => {
-                  e.preventDefault()
-                  update(state => ({ open: !state.open }))
-                }}
-                title='Menu'>
-                <Burger />
-              </MenuButton>
-            )}
-            <Box
-              {...props}
-              mx='auto'
-              px={4}
-              py={4}
-              width={1}
-              style={{
-                maxWidth: 768,
-                minHeight: '100vh',
-              }}>
-              {props.children}
-              <Pagination {...site.siteMetadata} />
-            </Box>
-          </Sidebar>
-        )}
-      />
+  <StaticQuery
+    query={query}
+    render={({ site }) => (
+      <Flex>
+        <Sidepane>
+          <Box
+            width={[ 256 ]}
+            px={2}
+            py={3}
+            bg='white'
+            style={{
+              minHeight: '100vh'
+            }}>
+            {site.siteMetadata.navigation.map(({ text, href }) => (
+              <NavLink
+                key={href}
+                to={href}
+                children={text}
+              />
+            ))}
+            <Box my={4} />
+            <NavLink href='https://github.com/rebassjs/rebass' children='GitHub' />
+            <NavLink href='https://jxnblk.com' children='Made by Jxnblk' />
+          </Box>
+        </Sidepane>
+        <Box
+          {...props}
+          mx='auto'
+          px={4}
+          py={4}
+          width={1}
+          style={{
+            maxWidth: 768,
+            minHeight: '100vh',
+          }}>
+          {props.children}
+          <Pagination {...site.siteMetadata} />
+        </Box>
+      </Flex>
     )}
   />
